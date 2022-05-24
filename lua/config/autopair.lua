82a92f388ee2e84515f0a94b6af82dc49d69f77d
Nathan <79507956+CUexter@users.noreply.github.com>
2022-05-24 18:43:39 +0800

    fix(autopair): :bug: fixed autopair backspace not working

--- lua/config/autopair.lua
+++ lua/config/autopair.lua
@@ -1,37 +1,38 @@
 local remap = vim.api.nvim_set_keymap
-local npairs = require('nvim-autopairs')
+local npairs = require("nvim-autopairs")
 
 npairs.setup({ map_bs = false, map_cr = false })
 
 vim.g.coq_settings = { keymap = { recommended = false } }
 
 -- these mappings are coq recommended mappings unrelated to nvim-autopairs
-remap('i', '<esc>', [[pumvisible() ? "<c-e><esc>" : "<esc>"]], { expr = true, noremap = true })
-remap('i', '<c-c>', [[pumvisible() ? "<c-e><c-c>" : "<c-c>"]], { expr = true, noremap = true })
-remap('i', '<tab>', [[pumvisible() ? "<c-n>" : "<tab>"]], { expr = true, noremap = true })
-remap('i', '<s-tab>', [[pumvisible() ? "<c-p>" : "<bs>"]], { expr = true, noremap = true })
+remap("i", "<esc>", [[pumvisible() ? "<c-e><esc>" : "<esc>"]], { expr = true, noremap = true })
+remap("i", "<c-c>", [[pumvisible() ? "<c-e><c-c>" : "<c-c>"]], { expr = true, noremap = true })
+remap("i", "<tab>", [[pumvisible() ? "<c-n>" : "<tab>"]], { expr = true, noremap = true })
+remap("i", "<s-tab>", [[pumvisible() ? "<c-p>" : "<bs>"]], { expr = true, noremap = true })
 
 -- skip it, if you use another global object
-_G.MUtils= {}
+_G.MUtils = {}
 
 MUtils.CR = function()
-  if vim.fn.pumvisible() ~= 0 then
-    if vim.fn.complete_info({ 'selected' }).selected ~= -1 then
-      return npairs.esc('<c-y>')
-    else
-      return npairs.esc('<c-e>') .. npairs.autopairs_cr()
-    end
-  else
-    return npairs.autopairs_cr()
-  end
+	if vim.fn.pumvisible() ~= 0 then
+		if vim.fn.complete_info({ "selected" }).selected ~= -1 then
+			return npairs.esc("<c-y>")
+		else
+			return npairs.esc("<c-e>") .. npairs.autopairs_cr()
+		end
+	else
+		return npairs.autopairs_cr()
+	end
 end
-remap('i', '<cr>', 'v:lua.MUtils.CR()', { expr = true, noremap = true })
+remap("i", "<cr>", "v:lua.MUtils.CR()", { expr = true, noremap = true })
 
 MUtils.BS = function()
-  if vim.fn.pumvisible() ~= 0 and vim.fn.complete_info({ 'mode' }).mode == 'eval' then
-    return npairs.esc('<c-e>') .. npairs.autopairs_bs()
-  else
-    return npairs.autopairs_bs()
-  end
+	local bufnr = vim.api.nvim_get_current_buf()
+	if vim.fn.pumvisible() ~= 0 and vim.fn.complete_info({ "mode" }).mode == "eval" then
+		return npairs.esc("<c-e>") .. npairs.autopairs_bs(bufnr)
+	else
+		return npairs.autopairs_bs(bufnr)
+	end
 end
-remap('i', '<bs>', 'v:lua.MUtils.BS()', { expr = true, noremap = true })
+remap("i", "<bs>", "v:lua.MUtils.BS()", { expr = true, noremap = true })
