20fea81479e25936f383b8fbdf7126e3ef231bd7
Nathan <79507956+CUexter@users.noreply.github.com>
2022-06-04 05:37:18 +0800

    feat(telescope): esc to quit telescope instantly

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -43,7 +43,8 @@ telescope.setup({
 				["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
 				["<C-l>"] = actions.complete_tag,
 				["<C-_>"] = actions.which_key, -- keys from pressing <C-/>
-				-- ["<esc>"] = actions.close,
+				["<C-o>"] = { "<esc>", type = "command" },
+				["<esc>"] = actions.close,
 			},
 
 			n = {
