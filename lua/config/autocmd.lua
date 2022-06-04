964a839a0b6d877b1b4f6b30700135c18e77dd09
Nathan <79507956+CUexter@users.noreply.github.com>
2022-06-05 03:26:36 +0800

    feat(keymap): markdown preview key

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -13,8 +13,9 @@ vim.cmd([[
   augroup end
   augroup _markdown
     autocmd!
-    autocmd FileType markdown setlocal wrap
-    autocmd FileType markdown setlocal spell
+    autocmd FileType markdown,pandoc setlocal wrap
+    autocmd FileType markdown,pandoc setlocal spell
+    autocmd FileType markdown,pandoc lua whichkeyMarkdown()
   augroup end
   augroup _auto_resize
     autocmd!
@@ -29,3 +30,22 @@ vim.cmd([[
     autocmd FileType LspsagaHover,LspsagaCodeAction,LspsagaRename,LspsagaSignatureHelp,LspsagaFinder,LspsagaDiagnostic nnoremap <buffer><nowait><silent> <Esc> <cmd>close!<cr>
   augroup END
 ]])
+
+local opts = {
+	mode = "n", -- NORMAL mode
+	prefix = "<leader>",
+	buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
+	silent = true, -- use `silent` when creating keymaps
+	noremap = true, -- use `noremap` when creating keymaps
+	nowait = true, -- use `nowait` when creating keymaps
+}
+_G.whichkeyMarkdown = function()
+	local wk = require("which-key")
+	local buf = vim.api.nvim_get_current_buf()
+	wk.register({
+		m = {
+			name = "Markdown",
+			p = { "<cmd>MarkdownPreviewToggle<cr>", "Toggle Preview", buffer = buf },
+		},
+	}, opts)
+end
