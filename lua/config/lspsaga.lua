816f63bfa2569189ea8fa7230939a7122ddc8f34
CUexter <1155143289@link.cuhk.edu.hk>
2023-05-15 21:39:35 +0800

    fix: nerdfont update

--- lua/config/lspsaga.lua
+++ lua/config/lspsaga.lua
@@ -3,13 +3,13 @@ lspsaga.setup({ -- defaults ...
 	debug = false,
 	use_saga_diagnostic_sign = true,
 	-- diagnostic sign
-	error_sign = "",
+	error_sign = "󰅚",
 	warn_sign = "",
-	hint_sign = "",
+	hint_sign = "󰌶",
 	infor_sign = "",
 	diagnostic_header_icon = "   ",
 	-- code action title icon
-	code_action_icon = " ",
+	code_action_icon = "󰌵 ",
 	code_action_prompt = {
 		enable = false,
 		-- sign = true,
@@ -35,7 +35,7 @@ lspsaga.setup({ -- defaults ...
 		quit = "<C-c>",
 		exec = "<CR>",
 	},
-	definition_preview_icon = "  ",
+	definition_preview_icon = "󰀹  ",
 	border_style = "single",
 	rename_prompt_prefix = "➤",
 	rename_output_qflist = {
